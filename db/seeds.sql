0172423fc60d5d2ffc2b6e0bc48805a6ff2c16fa
Ryan Ngoh <rxn120230@utdallas.edu>
2016-12-14 10:00:15 -0600

    added files to bub's burger

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain ol Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Extra Bun Burger');
+INSERT INTO burgers (burger_name) VALUES ('Grape Burger');
