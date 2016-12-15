201262028c75e7f0b4cd4b2a955e5bfb957a0448
Ryan Ngoh <rxn120230@utdallas.edu>
2016-12-15 11:40:22 -0600

    removed modals. They don't work :(

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,4 @@ INSERT INTO burgers (burger_name) VALUES ('Plain ol Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Extra Bun Burger');
 INSERT INTO burgers (burger_name) VALUES ('Grape Burger');
 INSERT INTO burgers (burger_name) VALUES ('Crocodile Steve Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bubba Gumbo Burger');
